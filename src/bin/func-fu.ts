752c6329bb089b3ae57568e91758994ca31ab0d5
Alexey Alexeyev <ne4to777@gmail.com>
2022-01-05 23:24:58 +0300

    patches

--- src/bin/func-fu.ts
+++ src/bin/func-fu.ts
@@ -1,19 +1,19 @@
-import {para, paraAsync, paraSync, pipe, pipeAsync, pipeSync, startMeasure} from '..';
+// import {para, paraAsync, paraSync, pipe, pipeAsync, pipeSync, startMeasure} from '..';
 // import {pipe, pipeAsync, pipeSync, startMeasure} from '../../dist';
 //
-const run = async () => {
-    const incAsync = async x => x + 1
-    const incSync = x => x + 1
-    const fs = []
-    for (let i = 0; i < 1e5; i += 1) {
-        fs[i] = i < 1 ? incSync : incSync
-    }
-    const stopMeasure = startMeasure()
-    console.log(
-        await para(...fs)(0)
-    )
-    stopMeasure()
-}
-
-run()
+// const run = async () => {
+//     const incAsync = async x => x + 1
+//     const incSync = x => x + 1
+//     const fs = []
+//     for (let i = 0; i < 1e5; i += 1) {
+//         fs[i] = i < 1 ? incSync : incSync
+//     }
+//     const stopMeasure = startMeasure()
+//     console.log(
+//         await para(...fs)(0)
+//     )
+//     stopMeasure()
+// }
+//
+// run()
 
